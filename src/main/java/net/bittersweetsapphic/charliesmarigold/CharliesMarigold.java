9d154dccd616a53cd3a2734ee8325aa651cbc53c
Charlie <abirdwholikestoeat@gmail.com>
2024-03-28 12:55:59 -0500

    removed a comment

--- src/main/java/net/bittersweetsapphic/charliesmarigold/CharliesMarigold.java
+++ src/main/java/net/bittersweetsapphic/charliesmarigold/CharliesMarigold.java
@@ -16,7 +16,6 @@ import org.slf4j.Logger;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(CharliesMarigold.MODID)
 public class CharliesMarigold {
-    // Define mod id in a common place for everything to reference
     public static final String MODID = "charliesmarigold";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
